202926220b074f9d6c17472fe1b67af2a5407a82
Aditya-Rohan-Singh <aditya.rohan.singh-1@ou.edu>
2022-11-04 19:45:56 +0000

    node file

--- code/pokemon.py
+++ code/pokemon.py
@@ -2,8 +2,8 @@
 import argparse
 
 
-trainer_list = ["\U0001F9D2", "\U0001F9D2", "\U0001F466", "\U0001F467", "\U0001F9B3", "\U0001F468", "\U0001F469", "\U0001F64D", "\U0001F475", "\U0001F9D3"]
-pokemon_list = []
+trainer_list = ["\U0001F480", "\U0001F9D2", "\U0001F466", "\U0001F467", "\U0001F9B3", "\U0001F468", "\U0001F469", "\U0001F64D", "\U0001F475", "\U0001F9D3"]
+pokemon_list = ["\U0001F412", "\U0001F98D", "\U0001F43A", "\U0001F98A", "\U0001F436", "\U0001F431", "\U0001F981", "\U0001F42E", "\U0001F437", "\U0001F42D"]
 
 
 if __name__ == '__main__':
@@ -17,7 +17,11 @@ if __name__ == '__main__':
     f.write("version: '3.7'\n\n")
     f.write("services:\n  server:\n    build: .\n    hostname: server\n    container_name: Server\n    networks:\n      - default")
     for idx in range(args.T):
-        row = "  client"+str(idx+1)+":\n    build: .\n    hostname: " + trainer_list[idx] + "\n    container_name: " + trainer_list[idx] + "\n    networks:\n      - default"
+        row = "\n  client"+str(idx+1)+":\n    build: .\n    hostname: " + trainer_list[idx] + "\n    container_name: " + trainer_list[idx] + "\n    networks:\n      - default"
+        f.write(row)
+
+    for idx1 in range(args.P):
+        row =  "\n  client"+str(args.T+idx1+1)+":\n    build: .\n    hostname: " + pokemon_list[idx1] + "\n    container_name: " + pokemon_list[idx1] + "\n    networks:\n      - default"
         f.write(row)
 
     f.close()
